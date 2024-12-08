75d2f4b9e0d20b489d5795343ed8812b6d127a4f
Anatolii Iumashev <anatolii.iumashev@gmail.com>
2024-12-08 16:42:58 +0300

    dd

--- htmxer.php
+++ htmxer.php
@@ -3,7 +3,7 @@
  * Plugin Name:       @ HTMXer
  * Plugin URI:        https://github.com/aiiddqd/htmxer
  * Description:       Add HTMX to WordPress
- * Version:           0.1.0
+ * Version:           0.1.241208
  * License:           GPL-2.0+
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       htmxer
@@ -13,10 +13,11 @@
 
 add_action('wp_enqueue_scripts', function(){
     // wp_enqueue_script('htmx', plugins_url('htmx.min.js', __FILE__), [], null);
-    wp_enqueue_script('htmx', 'https://unpkg.com/htmx.org@2.0.3', [], '2.0.3');
+    wp_enqueue_script('htmx', 'https://unpkg.com/htmx.org@2.0.3', [], '2.0.3', true);
     wp_script_add_data('htmx', 'integrity', 'sha384-0895/pl2MU10Hqc6jd4RvrthNlDiE9U1tWmX7WRESftEDRosgxNsQG/Ze9YMRzHq');
-    wp_enqueue_script('htmx-preload', 'https://unpkg.com/htmx-ext-preload@2.0.1/preload.js', ['htmx']);
 });
+
+require_once __DIR__ . '/includes/Settings.php';
 
 foreach (glob(__DIR__ . '/includes/*.php') as $file) {
     require_once $file;
--- includes/Settings.php
+++ includes/Settings.php
@@ -11,3 +11,67 @@ add_action('wp_head', function (){
 	echo '<meta name="htmx-config" content=\'' . json_encode($config) . '\'>';
 
 });
+
+Settings::init();
+
+class Settings {
+
+    public static $settings_group = 'htmxer';
+
+    public static $option_key = 'htmxer_config';
+
+    public static function init(){
+        add_action('admin_init', function(){
+            register_setting( self::$settings_group, self::$option_key ); 
+        }, 5);
+        
+        add_action('admin_menu', function() {
+            add_options_page(
+                $page_title = 'HTMX',
+                $menu_title = 'HTMX',
+                $capability = 'administrator',
+                $menu_slug = 'htmxer',
+                $callback = [__CLASS__, 'render_settings']
+            );
+        }, 5);
+    }
+
+    public static function get_settings_group(){
+        return self::$settings_group;
+    }
+
+    public static function get_option_key(){
+        return self::$option_key;
+    }
+
+    public static function get_form_field_name($key){
+        return sprintf('%s[%s]', self::$option_key, $key);
+    }
+    
+    public static function get($key = null){
+        if(empty($key)){
+            return get_option(self::$option_key) ?? [];
+        } else {
+            return get_option(self::$option_key)[$key] ?? null;
+        }
+    }
+
+    /**
+     * Add settings
+     */
+    public static function render_settings(){
+        ?>
+        <div class="wrap">
+            <h1><?= __('HTMX', 'htmxer') ?></h1>
+
+            <form method="post" action="options.php">
+                <?php settings_fields( self::$settings_group ); ?>
+                <?php do_settings_sections( self::$settings_group ); ?>
+                <?php submit_button(); ?>
+
+            </form>
+        </div>
+        <?php
+    }
+
+}
