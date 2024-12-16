2a8f9daf5f3155cffdfff4737a7d4632a5e46a5c
Antony I <1852897+aiiddqd@users.noreply.github.com>
2024-12-16 14:00:55 +0300

    API and hooks improved (#2)

--- README.md
+++ README.md
@@ -3,12 +3,28 @@ HTMX for WordPress
 
 
 # example
+
+## initial HTML with HTMX
 ```
-<div hx-get="/wp-json/app/v1/toolbar" hx-trigger="load delay:0s" hx-swap="outerHTML"></div>
-<script>
-    document.body.addEventListener('htmx:configRequest', function (event) {
-        event.detail.parameters['url'] = window.location.href;
-    });
-</script>Ï
+<div hx-get="/wp-json/htmxer/toolbar" hx-trigger="load delay:0s" hx-swap="outerHTML"></div>
 ```
 url https://github.com/aiiddqd/aab/blob/main/main.php
+
+## AJAX response
+```
+add_action('htmxer/toolbar', function ($context) {
+    echo 'Hello World';
+});
+```
+
+## Context (optional)
+
+```
+add_filter('htmxer/context', function ($context) {
+    global $wp;
+    $post_id = get_post()->ID ?? null;
+    $context['post_id'] = $post_id;
+    $context['url'] = site_url($wp->request);
+    return $context;
+});
+```
--- htmxer.php
+++ htmxer.php
@@ -1,16 +1,16 @@
-<?php 
+<?php
 /**
  * Plugin Name:       @ HTMXer
  * Plugin URI:        https://github.com/aiiddqd/htmxer
  * Description:       Add HTMX to WordPress
- * Version:           0.1.241208
  * License:           GPL-2.0+
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       htmxer
  * Domain Path:       /languages
+ * Version:           0.1.241216
  */
 
-
+use WP_REST_Request;
 
 require_once __DIR__ . '/includes/Settings.php';
 
@@ -21,3 +21,56 @@ foreach (glob(__DIR__ . '/includes/*.php') as $file) {
 foreach (glob(__DIR__ . '/includes/*/config.php') as $file) {
     require_once $file;
 }
+
+/**
+ * Example
+ * url https://example.site/wp-json/htmxer/some_template
+ * hook add_action('htmxer/some_template', 'callback');
+ */
+function render_htmxer_template(WP_REST_Request $request)
+{
+    $hook = $request->get_param('hook');
+    $context = $request->get_header('context') ?? [];
+    if ($context) {
+        $context = json_decode($context, true);
+    } else {
+        $context = [];
+    }
+
+    header('Content-Type: text/html');
+    do_action('htmxer/' . $hook, $context, $request);
+    exit;
+}
+
+add_action('rest_api_init', function () {
+
+    global $wp;
+
+    //hack to fix authentication
+    if (wp_is_serving_rest_request()) {
+        if (strpos($wp->request, 'wp-json/htmxer/') === 0) {
+            remove_filter('rest_authentication_errors', 'rest_cookie_check_errors', 100);
+        }
+    }
+
+    register_rest_route('htmxer', '/(?P<hook>[a-zA-Z0-9\-_]+)', [
+        'methods' => 'GET',
+        'callback' => 'render_htmxer_template',
+        'permission_callback' => '__return_true',
+    ]);
+});
+
+add_action('wp_footer', function () {
+    $context = apply_filters('htmxer/context', []);
+    if (empty($context)) {
+        return;
+    }
+    ?>
+    <script>
+        document.body.addEventListener('htmx:configRequest', function (event) {
+            event.detail.headers['context'] = '<?= json_encode($context) ?>';
+        });
+    </script>
+
+    <?php
+}, 555);
