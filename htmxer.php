7778c7b1f9e5f4e08e3642dae1f3e9354eb6d1ef
Anatolii Iumashev <anatolii.iumashev@gmail.com>
2024-12-16 13:49:16 +0300

    dd

--- htmxer.php
+++ htmxer.php
@@ -24,15 +24,21 @@ foreach (glob(__DIR__ . '/includes/*/config.php') as $file) {
 
 /**
  * Example
- * url https://example.site/wp-json/htmxer/v1/some_template
- * hook add_action('htmxer_action_some_template', 'callback');
+ * url https://example.site/wp-json/htmxer/some_template
+ * hook add_action('htmxer/some_template', 'callback');
  */
 function render_htmxer_template(WP_REST_Request $request)
 {
     $hook = $request->get_param('hook');
+    $context = $request->get_header('context') ?? [];
+    if ($context) {
+        $context = json_decode($context, true);
+    } else {
+        $context = [];
+    }
 
     header('Content-Type: text/html');
-    do_action('htmxer/action/' . $hook, $request);
+    do_action('htmxer/' . $hook, $context, $request);
     exit;
 }
 
@@ -42,13 +48,12 @@ add_action('rest_api_init', function () {
 
     //hack to fix authentication
     if (wp_is_serving_rest_request()) {
-        if (strpos($wp->request, 'wp-json/htmxer/v1/') === 0) {
+        if (strpos($wp->request, 'wp-json/htmxer/') === 0) {
             remove_filter('rest_authentication_errors', 'rest_cookie_check_errors', 100);
         }
     }
 
-    // /wp-json/app/v1/toolbar
-    register_rest_route('htmxer', '/v1/(?P<hook>[a-zA-Z0-9\-_]+)', [
+    register_rest_route('htmxer', '/(?P<hook>[a-zA-Z0-9\-_]+)', [
         'methods' => 'GET',
         'callback' => 'render_htmxer_template',
         'permission_callback' => '__return_true',
@@ -57,6 +62,9 @@ add_action('rest_api_init', function () {
 
 add_action('wp_footer', function () {
     $context = apply_filters('htmxer/context', []);
+    if (empty($context)) {
+        return;
+    }
     ?>
     <script>
         document.body.addEventListener('htmx:configRequest', function (event) {
