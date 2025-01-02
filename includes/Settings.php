254b35da2f206c645de96de6d8b3496e4217057e
Anatolii Iumashev <anatolii.iumashev@gmail.com>
2025-01-02 17:57:39 +0300

    dd

--- includes/Settings.php
+++ includes/Settings.php
@@ -4,10 +4,7 @@ namespace htmxer;
 
 add_action('wp_head', function (){
     $config = apply_filters('htmxer/config', []);
-    // $config['includeIndicatorStyles'] = false;
-    
 	echo '<meta name="htmx-config" content=\'' . json_encode($config) . '\'>';
-
 });
 
 Settings::init();
