af28fb0fe143c7bb2c7c923ffb555ddbc03067ed
Anatolii Iumashev <anatolii.iumashev@gmail.com>
2025-03-31 09:34:36 +0300

    dd

--- includes/Assets.php
+++ includes/Assets.php
@@ -13,13 +13,13 @@ class Assets
     {
         add_action('wp_enqueue_scripts', function () {
             // wp_enqueue_script('htmx', plugins_url('htmx.min.js', __FILE__), [], null);
-            wp_enqueue_script('htmx', 'https://unpkg.com/htmx.org@2.0.3', [], '2.0.3',
+            wp_enqueue_script('htmxer', 'https://unpkg.com/htmx.org@2.0.3', [], null,
                 [
                     'strategy' => 'defer',
                     'in_footer' => true
                 ]
             );
-            wp_script_add_data('htmx', 'integrity', 'sha384-0895/pl2MU10Hqc6jd4RvrthNlDiE9U1tWmX7WRESftEDRosgxNsQG/Ze9YMRzHq');
+            // wp_script_add_data('htmxer', 'integrity', 'sha384-0895/pl2MU10Hqc6jd4RvrthNlDiE9U1tWmX7WRESftEDRosgxNsQG/Ze9YMRzHq');
         });
 
     }
